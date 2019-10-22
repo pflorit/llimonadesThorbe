8d96e8149ab8f0613f32d26b4f99325f035f796b
pflorit <pfloirt@nigul.cide.es>
2019-10-22 15:23:13 +0200

    comit de todo. patatilla

---
+++ llimonadesThorbe/main.py
@@ -0,0 +1,11 @@
+class Main:
+    def mostrar_menu(self):
+        print("======== Thorbe Systems ========")
+        print("[                              ]")
+        print("[     1· Iniciar sessio        ]")
+        print("[     2· Nou usuari            ]")
+        print("[     3· Sortir                ]")
+        print("[                              ]")
+        print("================================")
+        i = int(input("Seleccione una opcion (numero de lista): "))
+
