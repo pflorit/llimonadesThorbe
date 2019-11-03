fc9bd7603c8633bf78e7e0839246034a306f5c46
pflorit <45623457+pflorit@users.noreply.github.com>
2019-11-03 13:21:09 +0100

    casi acabado
    queda mostrar bonito la parte accounting

--- llimonadesThorbe/main.py
+++ llimonadesThorbe/main.py
@@ -25,6 +25,7 @@ def mostrar_menu_login(bd):
 
     elif i == "3":
         print("See you later!")
+        bd.close()
 
     else:
         mostrar_menu_login(bd)
@@ -45,16 +46,17 @@ def mostrar_menu_inicio(bd):
         sell_lemondae(bd)
 
     elif i == "2":
-        print("to do")
+        order_ingredients(bd)
 
     elif i == "3":
         make_lemondae(bd)
 
     elif i == "4":
-        print("to do")
+        see_accounting(bd)
 
     elif i == "5":
         print("See you later!")
+        bd.close()
 
     else:
         mostrar_menu_inicio(bd)
@@ -65,9 +67,25 @@ def create_connection(ip, nombre):
         host=ip,
         user="root",
         passwd="",
+        port="3307",
         database=nombre
     )
     return connection
+
+
+def see_accounting(bd):
+    micursor = bd.cursor()
+    micursor.execute("SELECT `date`,sum(`losses`)as total_lost from `accounting` group by `date`")
+    result = micursor.fetchall()
+    print("Losses per day:")
+    for res in result:
+        print(res)
+    micursor.execute("SELECT `date`,sum(`profit`)as total_won from `accounting` group by `date`")
+    result = micursor.fetchall()
+    print("Profits per day:")
+    for res in result:
+        print(res)
+    mostrar_menu_inicio(bd)
 
 
 def user_exists(micursor, username):
@@ -113,22 +131,33 @@ def logIn(username, pwd, bd):
 def make_lemondae(bd):
     liters = int(input("How many litters are you going to produce? "))
     li = int(liters)
-    micursor = bd.cursor()
-    micursor.execute("UPDATE `products` SET `total_amount`=`total_amount`+{} WHERE 1".format(li))
-    bd.commit()
-    print("*brrrrr Ding!* Lemonade produced.")
+    lemons = 4 * li
+    water = li
+    sugar = 175 * li
+    canProduce = remaining_products(bd, lemons, water, sugar)
+    if canProduce:
+        micursor = bd.cursor()
+        micursor.execute("UPDATE `products` SET `total_amount`=`total_amount`+{} WHERE 1".format(li))
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`-{} WHERE name=\"lemons\"".format(lemons))
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`-{} WHERE name=\"sugar\"".format(sugar))
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`-{} WHERE name=\"water\"".format(water))
+        bd.commit()
+        print("*brrrrr Ding!* Lemonade produced.")
     mostrar_menu_inicio(bd)
 
 
 def sell_lemondae(bd):
-    liters = int(input("How many litters do you want? "))
+    liters = int(input("How many liters do you want? (4€ per liter)"))
     r = remaining_lemonade(bd)
     li = int(liters)
+    money = li * 4
     if li > r or r == 0:
         print("Sorry, there's only {}L left".format(r))
     else:
         micursor = bd.cursor()
         micursor.execute("UPDATE `products` SET `total_amount`=`total_amount`-{} WHERE 1".format(li))
+        micursor.execute("INSERT INTO `accounting`(`id`, `profit`, `losses`, `date`) VALUES (NULL,{},NULL,CURRENT_DATE)"
+                         .format(money))
         bd.commit()
         print("Transaction complete.")
     mostrar_menu_inicio(bd)
@@ -142,11 +171,67 @@ def remaining_lemonade(bd):
     return int(remains)
 
 
-def remaining_products(bd):
+def remaining_products(bd, lemonsNeed, waterNeed, sugarNeed):
     micursor = bd.cursor()
-    micursor.execute("SELECT * FROM `products`")
-    remains = micursor.fetchall()
-    return remains
+    micursor.execute("SELECT `stock` FROM `ingredients` WHERE name=\"lemons\"")
+    lemons = str(micursor.fetchall()[0]).replace("(", "")
+    lemons = lemons.replace(",)", "")
+
+    micursor.execute("SELECT `stock` FROM `ingredients` WHERE name=\"water\"")
+    water = str(micursor.fetchall()[0]).replace("(", "")
+    water = water.replace(",)", "")
+
+    micursor.execute("SELECT `stock` FROM `ingredients` WHERE name=\"sugar\"")
+    sugar = str(micursor.fetchall()[0]).replace("(", "")
+    sugar = sugar.replace(",)", "")
+
+    if int(lemons) < int(lemonsNeed) or int(water) < int(waterNeed) or int(sugar) < int(sugarNeed):
+        print("Sorry, you don't have enough products.")
+        print("     Lemons: Need-> {}     Have-> {}".format(lemonsNeed, lemons))
+        print("     Water:  Need-> {}L    Have-> {}L".format(waterNeed, water))
+        print("     Sugar:  Need-> {}G    Have-> {}G".format(sugarNeed, sugar))
+        return False
+    else:
+        return True
+
+
+def order_ingredients(bd):
+    micursor = bd.cursor()
+    lemons = int(input("How many lemons do you want? "))
+    sugar = int(input("How many grams of sugar do you want? "))
+    water = int(input("How many liters of water do you want? "))
+    lemon_price = "{0:.2f}".format((lemons * 0.30))
+    water_price = "{0:.2f}".format((water * 0.59))
+    sugar_price = "{0:.2f}".format((sugar * 0.07))
+    total = float(lemon_price) + float(water_price) + float(sugar_price)
+    proceed = create_bill(lemon_price, water_price, sugar_price, total)
+    if proceed:
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`+{} WHERE `name`= \"lemons\"".format(lemons))
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`+{} WHERE `name`= \"sugar\"".format(sugar))
+        micursor.execute("UPDATE `ingredients` SET `stock`=`stock`+{} WHERE `name`= \"water\"".format(water))
+        micursor.execute("INSERT INTO `accounting`(`id`, `profit`, `losses`, `date`) VALUES (null,null,{},CURRENT_DATE)"
+                         .format(total))
+        bd.commit()
+    else:
+        print("Order canceled")
+    mostrar_menu_inicio(bd)
+
+
+def create_bill(lemons, water, sugar, total):
+    print()
+    print("======== ORDER ========")
+    print("Lemons: " + str(lemons) + "€")
+    print("Water: " + str(water) + "€")
+    print("Sugar: " + str(sugar) + "€")
+    print()
+    print("====> Final price: " + str(total) + "€")
+    proceed = ""
+    while proceed.upper() != "S" or proceed.upper() != "N":
+        proceed = input("Do you want to proceed?(S or N)")
+        if proceed.upper() == "S":
+            return True
+        elif proceed.upper() == "N":
+            return False
 
 
 mydb = create_connection("localhost", "lemondb")
